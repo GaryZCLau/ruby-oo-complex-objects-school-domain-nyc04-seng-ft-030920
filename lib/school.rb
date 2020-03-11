454d2620e1383ff2b0ae38c4a82ca2322d052520
Gary Lau <gareeeliu@gmail.com>
2020-03-11 19:56:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    roster = {}
+    @roster = roster
+  end
+  
+  def add_student(name, roster)
+    if @roster.keys.include?(roster)
+      @roster[roster] << name
+    else
+      @roster[roster] = [] 
+      @roster[roster] << name
+    end
+  end
+  
+  def grade(roster)
+    return @roster[roster]
+  end
+  
+  def sort
+    @roster.keys.sort
+  end
+end
